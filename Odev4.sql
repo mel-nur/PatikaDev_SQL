55a24c86cae7b86b0cb1b9f67cca092127093d8a
Melike Nur Çotak <115100660+mel-nur@users.noreply.github.com>
2024-06-21 23:27:09 +0300

    Update Odev4.sql

--- Odev4.sql
+++ Odev4.sql
@@ -1,19 +1,18 @@
---SELECT DISTINCT(replacement_cost) FROM film;
+--country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+SELECT DISTINCT(replacement_cost) FROM film;
 
---SELECT COUNT(DISTINCT(replacement_cost)) FROM film;
+--film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+SELECT COUNT(DISTINCT(replacement_cost)) FROM film;
 
-/*
+--film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
 SELECT COUNT(*) FROM film 
 WHERE title LIKE 'T%' AND rating='G';
-*/
 
-/*
+--country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
 SELECT COUNT(*) FROM country
 WHERE LENGTH(country)=5;
-*/
 
-/*
+--city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
 SELECT COUNT(*) FROM City 
 WHERE city ILIKE '%R';
-*/
 
